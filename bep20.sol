f0b9c1f2de69f4a18cb42a2723e94141dad36f23
vanes2k <73784650+vanes2k@users.noreply.github.com>
2022-05-29 12:22:36 +0300

    Update bep20.sol

--- bep20.sol
+++ bep20.sol
@@ -38,8 +38,7 @@ interface IBEP20 {
 
 
 contract Context {
-  // Empty internal constructor, to prevent people from mistakenly deploying
-  // an instance of this contract, which should be used via inheritance.
+  
   constructor () internal { }
 
   function _msgSender() internal view returns (address payable) {
@@ -97,10 +96,10 @@ library SafeMath {
 
  
   function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-    // Solidity only automatically asserts when dividing by 0
+    
     require(b > 0, errorMessage);
     uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    
 
     return c;
   }
